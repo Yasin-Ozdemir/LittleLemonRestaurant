0bd5bff8817a86ba2b0bc6c1d4ea4f884bc010ce
Yasin-Ozdemir <yasinozdemirgs37@gmail.com>
2024-12-17 17:43:48 +0300

    Initial Commit

---
+++ LittleLemonRestaurant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemonRestaurant
+//
+//  Created by Yasin Özdemir on 17.12.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
